36c9befc6ffca1b8fc506b16bc57125e292de0fb
Shawn Hoover <shawn.hoover@gmail.com>
2021-04-16 12:02:09 -0400

    Add cache-control

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -3,8 +3,13 @@ set -e
 [[ -z "$S3_BUCKET" ]] && { echo "S3_BUCKET is required" ; exit 1; }
 [[ -z "$CF_DIST" ]] && { echo "CF_DIST is required" ; exit 1; }
 
+# It's a static static site
+TTL=604800
+
+# Sync all assets to S3. Display output and capture for CloudFront invalidation.
 aws s3 sync . s3://$S3_BUCKET/ \
     --no-progress \
+    --cache-control max-age=$TTL \
     --exclude "*" \
     --include "*.html" \
     --include "*.css" \
@@ -15,6 +20,7 @@ aws s3 sync . s3://$S3_BUCKET/ \
     --include "*.webmanifest" \
   | tee sync.log
 
+# Invalidate cache for synced assets
 sed -E -n "s/^upload: .\/(.+) to s3:\/\/$S3_BUCKET\/.*$/\1/p" sync.log \
   | tee upload-matches.log \
   | xargs -I {} -t aws cloudfront create-invalidation --distribution-id $CF_DIST --paths /{}
