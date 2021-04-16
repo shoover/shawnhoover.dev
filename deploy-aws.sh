29b433fdf7f38c709825fddf681a3e91020098be
Shawn Hoover <shawn.hoover@gmail.com>
2021-04-16 09:24:24 -0400

    Invalidate only synced paths
    - I would LOVE to match the same path in 'to s3://...' but sed on the Mac
      doesn't match a backreference
    - That said, why not just do this in an S3-triggered lambda?

--- deploy-aws.sh
+++ deploy-aws.sh
@@ -1,7 +1,10 @@
+set -e
+
 [[ -z "$S3_BUCKET" ]] && { echo "S3_BUCKET is required" ; exit 1; }
 [[ -z "$CF_DIST" ]] && { echo "CF_DIST is required" ; exit 1; }
 
 aws s3 sync . s3://$S3_BUCKET/ \
+    --no-progress \
     --exclude "*" \
     --include "*.html" \
     --include "*.css" \
@@ -9,6 +12,11 @@ aws s3 sync . s3://$S3_BUCKET/ \
     --include "*.pdf" \
     --include "*.png" \
     --include "*.ico" \
-    --include "*.webmanifest"
+    --include "*.webmanifest" \
+  | tee sync.log
 
-aws cloudfront create-invalidation --distribution-id $CF_DIST --paths "/*"
+sed -E -n "s/^upload: .\/(.+) to s3:\/\/$S3_BUCKET\/.*$/\1/p" sync.log \
+  | tee upload-matches.log \
+  | xargs -I {} -t aws cloudfront create-invalidation --distribution-id $CF_DIST --paths /{}
+
+rm sync.log upload-matches.log
