083ae82519b7d5437dd6997dbaaebfd2d143a23e
Shawn Hoover <shawn.hoover@gmail.com>
2021-04-18 11:20:47 -0400

    Simplify s3 sync
    - Everything in site/ is fair game

--- script/deploy-aws.sh
+++ script/deploy-aws.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 [[ -z "$S3_BUCKET" ]] && { echo "S3_BUCKET is required" ; exit 1; }
@@ -6,21 +8,13 @@ set -e
 # It's a static static site
 TTL=604800
 
-# Sync all assets to S3. Display output and capture for CloudFront invalidation.
+# Sync content to S3. Display output and capture for CloudFront invalidation.
 aws s3 sync site s3://$S3_BUCKET \
     --no-progress \
     --cache-control max-age=$TTL \
-    --exclude "*" \
-    --include "*.html" \
-    --include "*.css" \
-    --include "*.ttf" \
-    --include "*.pdf" \
-    --include "*.png" \
-    --include "*.ico" \
-    --include "*.webmanifest" \
   | tee sync.log
 
-# Invalidate cache for synced assets
+# Invalidate cache for synced files
 sed -E -n "s/^upload: site\/(.+) to s3:\/\/$S3_BUCKET\/.*$/\1/p" sync.log \
   | tee upload-matches.log \
   | xargs -I {} -t aws cloudfront create-invalidation --distribution-id $CF_DIST --paths /{}
