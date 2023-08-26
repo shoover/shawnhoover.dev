21b0d58930b740de615a04a91f0ed62392196c53
Shawn Hoover <shawn.hoover@gmail.com>
2023-08-26 13:39:34 -0400

    Batch CloudFront invalidations, invalidate deletes

--- script/deploy-aws.sh
+++ script/deploy-aws.sh
@@ -10,8 +10,6 @@ set -x # debug
 # It's a static static site
 TTL=604800
 
-aws sts get-caller-identity
-
 # Sync content to S3. Display output and capture for CloudFront invalidation.
 # Annoyingly, this syncs all files in CI. Live with it; I don't want to risk
 # using --size-only for text files.
@@ -22,8 +20,9 @@ aws s3 sync site s3://$S3_BUCKET \
   | tee sync.log
 
 # Invalidate cache for synced files
-sed -E -n "s/^upload: site\/(.+) to s3:\/\/$S3_BUCKET\/.*$/\1/p" sync.log \
+sed -E -n -e "s/^(upload: site\/.+ to|delete:) s3:\/\/$S3_BUCKET\/(.+)$/\/\2/p" sync.log \
+  | sed -e 's/ /%20/g' \
   | tee upload-matches.log \
-  | xargs -I {} -t aws cloudfront create-invalidation --distribution-id $CF_DIST --paths /{}
+  | xargs --no-run-if-empty --delimiter="\n" --exit -t aws cloudfront create-invalidation --distribution-id $CF_DIST --paths
 
 rm sync.log upload-matches.log
