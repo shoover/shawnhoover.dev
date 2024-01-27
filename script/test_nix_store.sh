aaf3f1a7f94419807609f2f5380c14b173696a2c
Shawn Hoover <shawn.hoover@gmail.com>
2024-01-27 09:44:41 -0500

    Pin setup-emacs@v6.0
    - This fixes the error in Cache - Put:
        error: path '/home/runner/.nix-profile/bin/emacs-29.1' is not in the Nix store
    - If we ever upgrade to a version with the changes currently on master, we may need
      to refresh the shell with a command like '. $HOME/.nix-profile/etc/profile.d/nix.sh'
      (or whatever path contains the profile script when installed for root)

--- script/test_nix_store.sh
+++ script/test_nix_store.sh
@@ -9,11 +9,18 @@ function start_container () {
 function container_1_bootstrap () {
     apt update
     apt upgrade -y
-    apt install -y curl xz-utils rsync
-    curl -o setup_emacs_install https://raw.githubusercontent.com/purcell/setup-emacs/master/install.sh
-    chmod u+x setup_emacs_install
-    mkdir -m 0755 /nix && chown root /nix
-    INPUT_VERSION=29.1 USER=root GITHUB_PATH=/tmp/github_path GITHUB_ENV=/tmp/github_env ./setup_emacs_install
+    apt install -y curl rsync sudo tree xz-utils
+
+    USER=nixuser
+    groupadd --gid 1000 $USER
+    useradd --system --uid 1000 --gid $USER --groups sudo --create-home --home-dir /home/$USER --shell /bin/bash $USER
+    echo "$USER ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+    su - $USER
+
+    curl -Lo nix.tar.gz https://github.com/purcell/setup-emacs/archive/refs/tags/v6.0.tar.gz
+    tar xf nix.tar.gz
+    INPUT_VERSION=29.1 GITHUB_PATH=/tmp/github_path GITHUB_ENV=/tmp/github_env setup-emacs-6.0/install.sh
+    # . $HOME/.nix-profile/etc/profile.d/nix.sh
     PATH="$HOME/.nix-profile/bin:/nix/var/nix/profiles/default/bin:$PATH"
     nix-store --query --requisites $(which emacs) | xargs -I {} rsync -av {} /mnt/data/nix_store/
 }
@@ -23,5 +30,7 @@ function container_2_restore () {
     apt install -y rsync
     mkdir -p /nix/store
     rsync -av /mnt/data/nix_store/ /nix/store/
-    emacs --version
+    emacs_store=$(find /nix/store -name "*-emacs-29-1")
+    test -x $emacs_store/bin/emacs
+    $emacs_store/bin/emacs --version
 }
